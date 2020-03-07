25ea05e2d3de3919f1ecfe689acfd8a8c81d3f60
1227430161 <1227430161@qq.com>
2020-03-07 17:48:29 +0800

    solve equation problem

---
+++ equation.py
@@ -0,0 +1,18 @@
+import math
+def solve(a,b,c):
+    d=b*b-4*a*c
+    if d>=0 and a!=0:
+        x1=(-b+math.sqrt(d))/(2*a)
+        x2=(-b-math.sqrt(d))/(2*a)
+        #print("x1=",x1,"x2=",x2)
+        return x1,x2
+    elif a==0:
+        x1=x2=-c/b
+        #print("x1=x2=",x1)
+        return x1,x2
+    else:
+        return("input is wrong!")
+a=float(input("please input a="))
+b=float(input("please input b="))
+c=float(input("please input c="))
+print(solve(a,b,c))
